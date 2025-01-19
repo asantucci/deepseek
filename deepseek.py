3a19c748f41a1b8e4a895fd28f44e3dd5873cf37
Liyuan <liyuanlacfo1990@gmail.com>
2025-01-19 10:43:40 -0800

    Create deepseek model

---
+++ deepseek.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+from moe import MoE
+from mla import MultiHeadLatentAttention
+from config import DeepSeekConfig
+import torch.nn.functional as F
+
+class Block(nn.Module):
+    def __init__(self, config: DeepSeekConfig):
+        super().__init__()
+        self.attn = MultiHeadLatentAttention(config)
+        self.moe = MoE(config)  
+    
+    def forward(self, x: torch.tensor):
+        x = x + self.attn(x)
+        moe_output, expert_load_balance_loss = self.moe(x)
+        x = x + moe_output
+        return x, expert_load_balance_loss
+    
+
+class DeepSeek(nn.Module):
+    def __init__(self, config: DeepSeekConfig):
+        super().__init__()
+        self.blocks = nn.ModuleList([Block(config) for _ in range(config.num_layers)])
+        self.lut = nn.Embedding(config.vocab_size, config.d_model)
+        self.lm_head = nn.Linear(config.d_model, config.vocab_size, bias=False)
+        self.dropout = nn.Dropout(config.dropout)
+        self.block_size = config.block_size
+        
+    def forward(self, x: torch.tensor, targets: torch.tensor=None) -> torch.tensor:
+        '''
+        Args:
+            x: (B, T)
+            target: (B, T)
+        return: (B, T, vocab_size)
+        '''
+        B, T = x.shape
+        assert T <= self.block_size, f"Sequence length {T} cannot exceed block size {self.block_size}"
+        
+        x = self.lut(x)
+        x = self.dropout(x)
+        block_loss = 0.
+        for block in self.blocks:
+            x, b_loss = block(x)
+            block_loss += b_loss if b_loss is not None else 0.
+        
+        if targets is not None:
+            logits = self.lm_head(x)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+            loss += block_loss
+        else:
+            # for inference, only the last token logits is used for prediction the next token
+            logits = self.lm_head(x[:, [-1], :])
+            loss = None
+        return logits, loss
+
+
+if __name__ == "__main__":
+    config = DeepSeekConfig(
+        d_model=1024,
+        nheads=128,
+        block_size=512,
+        dropout=0.0,
+        device="cuda",
+        use_kv_cache=True,
+        q_lora_rank=1536,
+        kv_lora_rank=512,
+        rope_head_dim=64,
+        nope_head_dim=128,
+        num_shared_experts=3,
+        total_num_experts=5,
+        hidden_dimension=20,
+        num_smaller_experts_per_expert=2,
+        num_activated_experts=4,
+        epsilon=1e-9,
+        expert_load_balance_factor=0.01,
+        vocab_size=10000,
+        num_layers=2,
+    )
+    input = torch.randint(0, config.vocab_size, (2, 2)).to(config.device)
+    targets = torch.randint(0, config.vocab_size, (2, 2)).to(config.device)
+    model = DeepSeek(config).to(config.device)
+    output, loss = model(input, targets)
+    print(f"when targets is not None: output shape: {output.shape}, loss: {loss}")
+    targets = None
+    model.eval()
+    output, loss = model(input, targets)
+    print(f"when targets is None: output shape: {output.shape}, loss: {loss}")
+    for name, module in model.named_modules():
+            print(f"{name}: {module}")
